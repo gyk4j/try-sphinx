31c06a41ddd7861140327b14f0a7ea7ac55d558c
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-11-29 21:21:53 +0800

    Change to classic theme

--- source/conf.py
+++ source/conf.py
@@ -24,5 +24,31 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'classic'
+html_theme_options = {
+    "rightsidebar" : False,
+    "stickysidebar" : False,
+    "collapsiblesidebar" : False,
+    "externalrefs" : False,
+#    "footerbgcolor" : "#000000",
+#    "footertextcolor" : "#000000",
+#    "sidebarbgcolor" : "#000000",
+#    "sidebarbtncolor" : "#000000",
+#    "sidebartextcolor" : "#000000",
+#    "sidebarlinkcolor" : "#000000",
+#    "relbarbgcolor" : "#000000",
+#    "relbartextcolor" : "#000000",
+#    "relbarlinkcolor" : "#000000",
+#    "bgcolor" : "#000000",
+#    "textcolor" : "#000000",
+#    "linkcolor" : "#000000",
+#    "visitedlinkcolor" : "#000000",
+#    "headbgcolor" : "#000000",
+#    "headtextcolor" : "#000000",
+#    "headlinkcolor" : "#000000",
+#    "codebgcolor" : "#000000",
+#    "codetextcolor" : "#000000",
+#    "bodyfont" : "#000000",
+#    "headfont" : "#000000",
+}
 html_static_path = ['_static']
